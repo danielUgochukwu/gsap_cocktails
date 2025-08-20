3170a5f41c1736c8c8804cf6558407f3e26675f7
Daniel Ugochukwu <slade8546@gmail.com>
2025-08-20 14:34:21 +0100

    Implement contact section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,77 @@
+import {openingHours, socials} from "../../constants/index.js";
+import gsap from "gsap";
+import {useGSAP} from "@gsap/react";
+import { SplitText } from "gsap/all";
+
+
+const Contact = () => {
+    useGSAP(() => {
+        const titleSplit = SplitText.create('#contact h2', { type: 'words' });
+
+        const timeLine = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#contact',
+                start: 'top center'
+            },
+            ease: 'power1.out',
+        })
+
+        timeLine
+            .from(titleSplit.words, {
+                opacity: 1, yPercent: 100, stagger: 0.02
+            })
+            .from('#contact h3, #contact p', {
+                opacity: 1, yPercent: 100, stagger: 0.02
+            })
+            .to('#f-right-leaf', {
+                y: '-50', duration: 1, ease: 'power1.out',
+            })
+            .to('#f-left-leaf', {
+                y: '-50', duration: 1, ease: 'power1.out',
+            }, '<')
+    })
+
+    return (
+        <footer id={`contact`}>
+            <img src={`/images/footer-right-leaf.png`} alt={`leaf-right`} id={`f-right-leaf`}/>
+            <img src={`/images/footer-left-leaf.png`} alt={`leaf-left`} id={`f-left-leaf`}/>
+
+            <div className={`content`}>
+                <h2>Where to Find Us</h2>
+
+                <div>
+                    <h3>Visit Our Bar</h3>
+                    <p>456, Raq Blvd. #404, Los Angeles, CA 90210</p>
+                </div>
+
+                <div>
+                    <h3>Contact Us</h3>
+                    <p>(555) 987-645</p>
+                    <p>hello@cocotail.com</p>
+                </div>
+
+                <div>
+                    <h3>Opening Every Day</h3>
+                    {openingHours.map((time) => (
+                        <p key={time.day}>
+                            {time.day} : {time.time}
+                        </p>
+                    ))}
+                </div>
+
+                <div>
+                    <h3>Socials</h3>
+
+                    <div className={`flex-center gap-5`}>
+                        {socials.map((social) => (
+                            <a key={social.name} href={social.url} rel="noopener noreferrer" aria-label={social.name}>
+                                <img src={social.icon} alt={social.name} />
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+export default Contact
